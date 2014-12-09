880ba490e3922041853659f36fa4afcafe305614
nickozoulis <nickolas.zoulis@gmail.com>
2014-12-09 16:02:23 +0200

    first commits, structuring project

---
+++ data-crawler/src/main/java/Crawler.java
@@ -0,0 +1,53 @@
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Created by nickozoulis on 9/12/2014.
+ */
+public class Crawler extends WebCrawler {
+
+    static final Logger logger = LoggerFactory.getLogger(Crawler.class);
+
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g"
+            + "|png|tiff?|mid|mp2|mp3|mp4"
+            + "|wav|avi|mov|mpeg|ram|m4v|pdf"
+            + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+
+    /**
+     * This function decides whether the given URL should be crawled or not.
+     */
+    public boolean shouldVisit(WebURL url) {
+        String href = url.getURL().toLowerCase();
+        return !FILTERS.matcher(href).matches() && href.startsWith("http://www.ics.uci.edu/");
+    }
+
+    /**
+     * This function is called when a page is fetched and ready
+     * to be processed by your program.
+     */
+    @Override
+    public void visit(Page page) {
+        String url = page.getWebURL().getURL();
+        System.out.println("URL: " + url);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            System.out.println("Text length: " + text.length());
+            System.out.println("Html length: " + html.length());
+            System.out.println("Number of outgoing links: " + links.size());
+
+        }
+    }
+
+}
