3019080a677828a4fd3ce00a8e5972e19c0af458
nickozoulis <nickolas.zoulis@gmail.com>
2015-04-01 21:00:26 +0300

    minor changes to formatter

--- data-formatter/extract.sh
+++ data-formatter/extract.sh
@@ -9,19 +9,21 @@ while getopts ":i:" opt; do
 done
 #-->
 
-#<-- Extract the name of the course and the academic period. They will be used as filenames later.
-
-#-->
-
 #<-- Extract table data from files containing {studentId, grade, semester}
 # Foreach file in the input dicectory ending with '_'
-for file in ${input_dir}*_
+for file in ${input_dir}*
 do
 	# Extract data using regex and perform some cleansing.
-	grep -oP "<?xml(.*)</DIV>" $file | grep -oP "\">?(.*?)</" | awk '{print $2 $3}' | grep -oP ">(.*?)<" | sed 's/[<>]//g' > temp.txt
+	grep -oP "<?xml(.*)</DIV>" $file | grep -oP "\">?(.*?)</" | awk '{print $2 $3}' | grep -oP ">(.*?)<" | sed 's/[<>]//g' > temp
 	# Skip some empty lines in the beginning of the extracted file.
-	tail temp.txt -n +5 > ${file}_data.txt	
+	tail temp -n +5 > ${file}_data.txt	
 	# Remove the temporary file used to clean empty lines.
 	rm -f temp.txt
 done
 #-->
+
+# Create a directory to store the extracted data.
+DIR=${input_dir}"grades/"
+mkdir ${DIR}
+
+mv ${input_dir}*_data.txt ${DIR}
